46ca9795d4cee6c8fd94c833e632d2bac67d3bfc
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-08-23 17:07:53 +0530

    loadingbar added to home page

--- src/Components/Products/HomeProduct/HomeProduct.jsx
+++ src/Components/Products/HomeProduct/HomeProduct.jsx
@@ -2,14 +2,19 @@ import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { FaAngleRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import ClipLoader from "react-spinners/ClipLoader";
 function HomeProduct({type,title}){
     const [data,setData] = useState([]);
     const URL = 'http://localhost:5000';
+    const [homeProductLoadingBar , setHomeProductLoadingBar ] = useState(false);
     async function fetchData(){
         try {
+            setHomeProductLoadingBar(true);
             const response = await axios.get(`${URL}/products/?type=${type}`)
-            setData([...response.data.filteredproducts])
+            setData([...response.data.filteredproducts]);
+            setHomeProductLoadingBar(false);
         } catch (error) {
+            setHomeProductLoadingBar(false);
             console.log(error.message);
         }
     }
@@ -18,28 +23,38 @@ function HomeProduct({type,title}){
     },[])
     
     return (
-        <>  
-            <div className=" py-4 relative">
-                <div className="mx-[45px] flex justify-between items-center">
-                    <p  className="md:text-3xl font-semibold">Trending {title}</p>    
-                    <Link to={`/all/?type=${type}`}>
-                        <FaAngleRight className="text-white bg-blue-600 hover:bg-red-600 text-2xl rounded-2xl "/>
-                    </Link>                                   
-                </div>
-                <div className="flex justify-center">
-                    {data && data.slice(0,4).map((ele,idx)=>
-                        <Link key={idx} to={`/products/${ele._id}`} >
-                            <div  className="border-2 border-black  md:mx-2 mt-3 md:w-[300px] mx-[3px]  rounded-md p-3 hover:bg-gray-100">
-                                <img className="md:w-[300px] p-3" src={ele.ProductPhoto.secure_url} alt={ele.ProductName} />
-                                <hr />
-                                <p className="ml-2 md:text-lg text-[11px]">{ele.ProductName}</p>
-                                <p className="ml-2 md:text-lg md:font-bold ">₹{ele.ProductPrice}</p>
-                            </div>
-                        </Link>
-                    )}
-                </div>
-            </div>
-            <hr />
+        <>  {
+                homeProductLoadingBar ? ( 
+                    <div className="text-center my-4">
+                        <ClipLoader/>
+                    </div>
+
+                ) : (
+                <>
+                    <div className=" py-4 relative">
+                        <div className="mx-[45px] flex justify-between items-center">
+                            <p  className="md:text-3xl font-semibold">Trending {title}</p>    
+                            <Link to={`/all/?type=${type}`}>
+                                <FaAngleRight className="text-white bg-blue-600 hover:bg-red-600 text-2xl rounded-2xl "/>
+                            </Link>                                   
+                        </div>
+                        <div className="flex justify-center">
+                            {data && data.slice(0,4).map((ele,idx)=>
+                                <Link key={idx} to={`/products/${ele._id}`} >
+                                    <div  className="border-2 border-black  md:mx-2 mt-3 md:w-[300px] mx-[3px]  rounded-md p-3 hover:bg-gray-100">
+                                        <img className="md:w-[300px] p-3" src={ele.ProductPhoto.secure_url} alt={ele.ProductName} />
+                                        <hr />
+                                        <p className="ml-2 md:text-lg text-[11px]">{ele.ProductName}</p>
+                                        <p className="ml-2 md:text-lg md:font-bold ">₹{ele.ProductPrice}</p>
+                                    </div>
+                                </Link>
+                            )}
+                        </div>
+                    </div>
+                    <hr />
+                </>
+                )
+            }
         </>
     )
 };
